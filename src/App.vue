33a31d4e0310867258eefdda9874337e54c56d72
vfa.baothg <baothg@vitalify.asia>
2022-12-19 10:15:50 +0700

    Add dashboard UI of Uppy

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <dashboard :uppy="uppy" :plugins="['Webcam']" :props="{ theme: 'dark' }" />
+  </div>
+</template>
+
+<script>
+import { Dashboard } from "@uppy/vue";
+
+import "@uppy/core/dist/style.css";
+import "@uppy/dashboard/dist/style.css";
+import "@uppy/webcam/dist/style.css";
+import "@uppy/drag-drop/dist/style.css";
+import "@uppy/progress-bar/dist/style.css";
+
+import Uppy from "@uppy/core";
+import Webcam from "@uppy/webcam";
+import DragDrop from "@uppy/drag-drop";
+import ProgressBar from "@uppy/progress-bar";
+
+export default {
+  name: "App",
+  components: {
+    Dashboard,
+  },
+  computed: {
+    uppy: () => new Uppy().use(Webcam),
+  },
+  beforeUnmount() {
+    this.uppy.close({ reason: "unmount" });
+  },
+};
+</script>
---
+++ src/assets/components/FileUpload.vue
@@ -0,0 +1,44 @@
+<script>
+// import React from "react";
+// import Uppy from "@uppy/core";
+// import AwsS3Multipart from "@uppy/aws-s3-multipart";
+// import AwsS3 from "@uppy/aws-s3";
+
+// const uppy = new Uppy({
+//   id: "uppy",
+//   autoProceed: true,
+// });
+// uppy.use(AwsS3, {
+//   getUploadParameters(file) {
+//     const presignedUrl =
+//       "https://test-upload-multipart-baothg.s3.ap-northeast-1.amazonaws.com/my-object?AWSAccessKeyId=AKIA36DVPTPR7KQCPR6Z&Content-Type=binary%2Foctet-stream&Expires=1671340616&Signature=F6fnvSMTCTqNWeQ7fcDNpF9LcrM%3D";
+//     return Promise.resolve({
+//       method: "PUT",
+//       url: presignedUrl,
+//     });
+//   },
+// });
+
+// function handleUpload(event) {
+//   const input = event.target;
+//   if (input.files.length > 0) {
+//     console.log(input.files[0].name);
+//     uppy.addFile(input.file[0]);
+//   }
+//   // uppy.getPlugin("AwsS3").openModal();
+//   uppy.use(AwsS3, {
+//     getUploadParameters(file) {
+//       const presignedUrl =
+//         "https://test-upload-multipart-baothg.s3.ap-northeast-1.amazonaws.com/my-object?AWSAccessKeyId=AKIA36DVPTPR7KQCPR6Z&Content-Type=binary%2Foctet-stream&Expires=1671340616&Signature=F6fnvSMTCTqNWeQ7fcDNpF9LcrM%3D";
+//       return Promise.resolve({
+//         method: "PUT",
+//         url: presignedUrl,
+//       });
+//     },
+//   });
+// }
+</script>
+
+<template>
+  <h1>Test uppy</h1>
+</template>
